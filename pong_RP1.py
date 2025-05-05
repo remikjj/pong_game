222c5ae32115b609a1843c86733bee410b691562
Remigiusz <remik.jaruga@gmail.com>
2025-05-05 21:52:25 +0200

    Finished UDP socket for server - ball is now sent and recived

--- pong_RP1.py
+++ pong_RP1.py
@@ -37,49 +37,56 @@ def move_paddle(event, xpaddle, ypaddle):
     return xpaddle, ypaddle
 
 def bounce_ball(xball, yball, dx, dy, xpaddle, ypaddle):
-    # Zgaszenie starej pozycji piłki
-    if 0 <= xball < 8 and 0 <= yball < 8:
-        sense.set_pixel(xball, yball, black)
-
-    # Przesunięcie piłki
-    xball += dx
-    yball += dy
-
-    # Odbicie od góry / dołu
-    if yball < 0:
-        yball = 0
-        dy = -dy
-    elif yball > 7:
-        yball = 7
-        dy = -dy
-
-    # Sprawdzenie odbicia od paletki
-    if xball == xpaddle + 1:  # Bo paddle na lewo
-        if yball == ypaddle or yball == ypaddle + 1:
-            dx = -dx
-            dy = 1 if yball == ypaddle + 1 else -1
-        else:
-            # KONIEC GRY
-            sense.set_pixel(xball, yball, ball_color)
-            time.sleep(0.3)
-            sense.set_pixel(xball, yball, black)
-            sense.set_pixel(xball + dx, yball + dy, ball_color)
-            time.sleep(0.3)
-            sense.clear()
-            sense.show_message("GAME OVER", text_colour=(255, 0, 0))
-            exit()
-
-    # Odbicie od prawej/lewej krawędzi
-    elif xball > 7:
-        #xball = 7
-        #dx = dx
-        data = f"{0},{yball},{dx},{dy}".encode() # ostania pozycja pilki na polu RP1
-        server.sendto(data, client_address)
-        xball = -1 # Ukrycie pilki z pola RP1
-        
-    elif xball < 0:
-        xball = 0
-        dx = -dx
+    # TRYB NASLUCHIWANIA - czekaj na odbior pilki od RP2
+    if xball == -1:
+        try:
+            server.settimeout(0.1)
+            data, _ = server.recvfrom(1024)
+            xball, yball, dx, dy = map(int, data.decode().split(','))
+            
+            if dx == 1:
+                xball = 0 # RP2 odbiera z lewej
+            elif dx == -1:
+                xball = 7 # odbiera z prawej
+        except socket.timeout:
+            return -1, -1, dx, dy
+            
+    else: # TRYB ROZGRYWKI - pilka znajduje sie na polu
+        if 0 <= xball <= 7 and 0 <= yball <= 7:
+            if xball == 7 and dx == 1: # Koniec pola - pora wyslac pilke
+                data = f"{0},{yball},{dx},{dy}".encode()
+                server.sendto(data, client_address)
+                sense.set_pixel(xball, yball, black)
+                xball = -1 # Pilka znika - przelaczenie w tryb nasluchu
+            else:
+                # Ruch pilki
+                sense.set_pixel(xball, yball, black)
+                xball += dx
+                yball += dy
+                
+                # Odbicie od gory/dolu
+                if yball < 0:
+                    yball = 0
+                    dy = -dy
+                elif yball > 7:
+                    yball = 7
+                    dy = -dy
+                        
+                # Sprawdzenie odbicia od paletki
+                if xball == xpaddle + 1:  # Bo paddle na lewo
+                    if yball == ypaddle or yball == ypaddle + 1:
+                        dx = -dx
+                        dy = 1 if yball == ypaddle + 1 else -1
+                    else:	# KONIEC GRY
+                        sense.set_pixel(xball, yball, ball_color)
+                        time.sleep(0.3)
+                        sense.set_pixel(xball, yball, black)
+                        sense.set_pixel(xball + dx, yball + dy, ball_color)
+                        time.sleep(0.3)
+                        sense.clear()
+                        sense.show_message("GAME OVER", text_colour=(255, 0, 0))
+                        exit()
+                
 
     # Rysowanie piłki w nowym miejscu
     if 0 <= xball < 8 and 0 <= yball< 8:
