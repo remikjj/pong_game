9a622307872077f672df56ceb975428a9bb9bd4f
Remigiusz <remik.jaruga@gmail.com>
2025-05-06 20:21:50 +0200

    Fixed ball collision with paddle

--- pong_RP2.py
+++ pong_RP2.py
@@ -51,44 +51,45 @@ def bounce_ball(xball, yball, dx, dy, xpaddle, ypaddle):
                 xball = 7	# RP1 odbiera z prawej
         except socket.timeout:
             return -1, -1, dx, dy
+        
+        
  
     else: # TRYB ROZGRYWKI - pika znajduje sie na polu
+        if xball == xpaddle - 1:  # Bo paddle na prawo
+                if xball == xpaddle - 1 and ypaddle <= yball <= ypaddle + 1:
+                    dx = -dx
+                    dy = 1 if yball == ypaddle + 1 else -1
+                else:
+                    # KONIEC GRY
+                    sense.set_pixel(xball, yball, ball_color)
+                    time.sleep(0.3)
+                    sense.set_pixel(xball, yball, black)
+                    sense.set_pixel(xball + dx, yball + dy, ball_color)
+                    time.sleep(0.3)
+                    sense.clear()
+                    sense.show_message("GAME OVER", text_colour=(255, 0, 0))
+                    exit()
+
+        # Ruch pilki
+        sense.set_pixel(xball, yball, black) # Zgaszenie starej pozycji pilki
+        xball += dx
+        yball += dy
+ 
+        # Odbicie od góry / dołu
+        if yball < 0:
+            yball = 0
+            dy = -dy
+        elif yball > 7:
+            yball = 7
+            dy = -dy
+                
         if 0 <= xball <=7 and 0 <= yball <=7:
             if xball == 0 and dx == -1: # Wyslanie pilki
                 data = f"{7},{yball},{dx},{dy}".encode()
                 client.sendto(data, server_address)
                 sense.set_pixel(xball, yball, black) # Zgaszenie starej pozycji pilki
                 xball = -1 # Pilka znika - przelaczenie w tryb nasluchiwania 
-            else:
-                 # Ruch pilki
-                sense.set_pixel(xball, yball, black) # Zgaszenie starej pozycji pilki
-                xball += dx
-                yball += dy
- 
-                # Odbicie od góry / dołu
-                if yball < 0:
-                    yball = 0
-                    dy = -dy
-                elif yball > 7:
-                    yball = 7
-                    dy = -dy
- 
-                # Sprawdzenie odbicia od paletki
-                if xball == xpaddle - 1:  # Bo paddle na prawo
-                    if xball == xpaddle - 1 and ypaddle <= yball <= ypaddle + 1:
-                        dx = -dx
-                        dy = 1 if yball == ypaddle + 1 else -1
-                    else:
-                        # KONIEC GRY
-                        sense.set_pixel(xball, yball, ball_color)
-                        time.sleep(0.3)
-                        sense.set_pixel(xball, yball, black)
-                        sense.set_pixel(xball + dx, yball + dy, ball_color)
-                        time.sleep(0.3)
-                        sense.clear()
-                        sense.show_message("GAME OVER", text_colour=(255, 0, 0))
-                        exit()
- 
+                 
     # Rysowanie piłki w nowym miejscu
     if 0 <= xball < 8 and 0 <= yball< 8:
         sense.set_pixel(xball, yball, ball_color)
